2b09593801c41d8ed2e00d274ec0e70a96311e25
MajaDev1983 <majastipetic22@gmail.com>
2019-07-06 14:07:13 +0200

    vjezba memorija

---
+++ Source.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <memory>
+
+class Entity
+{
+public:
+	Entity();
+	~Entity();
+
+private:
+
+};
+
+
+int main()
+{
+	int* someNumber = (int*)malloc(sizeof(int));
+	free(someNumber);
+
+	int* newNumber = new int;
+	delete newNumber;
+
+	Entity* mallocEntity = (Entyity*)malloc(sizeof(Entity));
+	free(mallocEntity);
+
+	Entity* newEntity = new Entity;
+	delete newEntity;
+
+	//Malloc ne poziva konstruktor klase, free ne poziva destruktor klase 
+	//new poziva konstruktora i delete destruktora
+}
